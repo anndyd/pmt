4c631fafb80f1a2d8f2e52411e8a84dc54c28481
Anndy Dong <anndy.dong@sap.com>
2017-08-10 08:34:21 +0800

    add set password in ad, add web publish profile

--- WcfService/PmtService.svc.cs
+++ WcfService/PmtService.svc.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
@@ -28,6 +29,26 @@ namespace WcfService
         string IPmtService.DoAction(Employee emp)
         {
             return emp.action + " [" + emp.empId + "] @ the " + Environment.MachineName;
+        }
+
+        public string GetEmployeeFromAD(string empId)
+        {
+            using (var ctx = new PrincipalContext
+         (ContextType.Domain,
+          "global.corp.sap",
+          "CN=I068054,OU=I,OU=Identities,DC=global,DC=corp,DC=sap",
+          "i068054@global.corp.sap",
+          "Carol990")) //set the super user in this section. 
+
+            {
+                using (var tstUser = UserPrincipal.FindByIdentity
+                       (ctx, IdentityType.SamAccountName, "i068054"))  //set the destination user 
+                {
+                    //tstUser.ChangePassword("Carol990", "G$tao123");
+                    tstUser.SetPassword("Initial1"); //the super user need full permission on OU
+                }
+            }
+            return "";
         }
     }
 }
